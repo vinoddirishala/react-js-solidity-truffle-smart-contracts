dcab5722f0dea7c5c1baf600e7dc109388ca2780
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-11-19 11:53:56 +0530

    basic project skelton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3'
+import React,{Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+  componentWillMount(){
+    this.loadBlockChainData()
+  }
+
+  async loadBlockChainData(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:3000")
+    const network = await web3.eth.net.getNetworkType()
+    const account = await web3.eth.getAccounts()
+    console.log(network)
+    console.log("account :"+account[0])
+    this.setState( {walletAddress:account[0]})
+
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {walletAddress:''}
+  }
+
+  render(){
+    return(
+      <div className="container">
+          <h2>Connected wallet address : {this.state.walletAddress}</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
