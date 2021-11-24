ef2b8b22703dab5c1af84c27a02355686b4c0c84
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-11-24 20:21:48 +0530

    Check metamask installation and asking for signing logics added in this commit

--- src/App.js
+++ src/App.js
@@ -4,33 +4,62 @@ import React,{Component} from 'react'
 
 class App extends Component{
 
-
-  componentWillMount(){
-    this.loadBlockChainData()
-  }
-
-  async loadBlockChainData(){
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:3000")
-    const network = await web3.eth.net.getNetworkType()
-    const account = await web3.eth.getAccounts()
-    console.log(network)
-    console.log("account :"+account[0])
-    this.setState( {walletAddress:account[0]})
-
-  }
-
   constructor(props){
-    super(props)
-    this.state = {walletAddress:''}
+    super(props);
+    this.state = {isWalletConnected:false,walletAddress:''};
   }
 
   render(){
     return(
       <div className="container">
-          <h2>Connected wallet address : {this.state.walletAddress}</h2>
+          {this.state.isWalletConnected ? 
+          (<h3>Connected to {this.state.walletAddress}</h3>)
+          :
+          (
+            
+          <button onClick={async () => {await this.connectToMetaMask();} }>Connect to wallet</button>
+          )}
       </div>
-    )
-  }
+    )}
+
+
+    async connectToMetaMask() {
+      //check metamask installed or not
+      if (window.ethereum) {
+        this.askForMetaMaskSigning();
+      }else{
+        this.setState = {isWalletConnected:false}
+        this.showInstallMetaMaskAlert();
+      }
+    }
+
+    async showInstallMetaMaskAlert(){
+      console.log("Install metamask");
+    }
+
+
+    async askForMetaMaskSigning(){
+      let ethereum = window.ethereum;
+      try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        this.setState = {isWalletConnected:true}
+        this.setState = {walletAddress:account[0]}
+        console.log(account);
+        
+        } catch {
+        
+          this.setState = {isWalletConnected:false}
+          this.setState = {walletAddress:''}
+        }
+
+  
+    }
+
+
 }
+
+
+
 
 export default App;
