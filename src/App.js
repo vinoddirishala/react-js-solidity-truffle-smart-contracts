6eae085f5340207b46128a802ce29c4f836e048a
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-11-25 23:10:00 +0530

    ++sign accounts requests. ++network check ++account change in   metamask listeners ++network change listeners added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Web3 from 'web3'
 import React,{Component} from 'react'
+const config = require('./config.json');
+
 
 class App extends Component{
+
+
 
   constructor(props){
     super(props);
@@ -42,10 +46,32 @@ class App extends Component{
     async askForMetaMaskSigning(){
       let ethereum = window.ethereum;
       try {
+        // ask for metamask sign transaction
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
+
+        const netWork = await ethereum.request({method:'net_version'});
+        if (netWork == config.networks['matic-testnet'].netWorkID) {
+          console.log("Perfect");
+        }else{
+          console.log("Switch network to "+config.networks['matic-testnet'].netWorkName);
+        }
+
+         // detect Network account change
+        ethereum.on('networkChanged', function(networkId){
+           console.log('networkChanged',networkId);
+        });
+
+          // listener to detect the account change
+        ethereum.on('accountsChanged', function (accounts) {
+          console.log('accountsChanges',accounts);
+    
+        });
+
         this.state.isWalletConnected = true;
         this.state.walletAddress = account;
+
+
         
         
         } catch {
